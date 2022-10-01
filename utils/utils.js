495003c852712db5190db61a1e12c2b8dcc08f34
hsaginor <hsaginor@gmail.com>
2022-09-30 20:48:39 -0700

    Implement pagination to get products.

--- utils/utils.js
+++ utils/utils.js
@@ -1,20 +1,36 @@
 const PRODUCTS_URL = '/products/test-product-list-ballmarker.json';
 
-export async function loadProducts(url = PRODUCTS_URL) {
-    const resp = await fetch(url);
+export async function loadProducts(url = PRODUCTS_URL, offset=0, limit=1000) {
+    console.log("loading products from " + url+'?offset='+offset+'&limit='+limit);
+    const resp = await fetch(url+'?offset='+offset+'&limit='+limit);
     if (!resp.ok) return {};
     const json = await resp.json();
-    return json.data;
+    return json;
 }
 
 export async function getProduct(productID) {
-    const productData = await loadProducts();
-    for (let i in productData) {
-        var p = productData[i];
-        if (p['Product ID'] === productID) {
-            return p;
+    var json = await loadProducts();
+    if(!json.data) return;
+
+    var totalRecords = json.total;
+    var count = 0;
+    
+    do {
+        let productData = json.data;
+        for (let i in productData) {
+            var p = productData[i];
+            if (p['Product ID'] === productID) {
+                console.log("found product [" + productID + "] after offset " + json.offset);
+                return p;
+            }
+            count++;
         }
-    }
+
+        if (count < totalRecords) {
+            let offset = json.offset + json.limit;
+            json = await loadProducts(PRODUCTS_URL, offset);
+        }
+    } while(count < totalRecords);
 }
 
 export function createTag(tag, attributes, html) {
