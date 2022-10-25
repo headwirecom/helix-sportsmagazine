515621d39d9119dee9fabf72b1a7c6f7470255f4
hsaginor <hsaginor@gmail.com>
2022-10-24 18:40:42 -0700

    load leaderboard players from service

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,6 +1,43 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
 import { leaderboardTemplate } from '../templates.js';
-import { createTagFromString } from '../../utils/utils.js';
+import { loadJsonData } from '../../utils/utils.js';
+
+const LEADERBOARD_URL = '/bin/leaderboard/leaders.json';
+
+function createLeaderboardPlayerHTML(data) {
+  let html = `
+  <div class="o-Leaderboard__m-Players__m-Player">
+      <div class="o-Leaderboard__m-Players__m-PlayerMain">
+          <h4 class="o-Leaderboard__m-Players__m-PlayerMain__a-Place">${data.Position}</h4>
+          <div class="o-Leaderboard__m-Players__m-PlayerMain__a-Name">
+            <i style="background-image:url(${data.CountryFlag}});"></i><h5>${data.FirstName} ${data.LatsName}</h5>
+          </div>
+          <p class="o-Leaderboard__m-Players__m-PlayerMain__a-Record">${data.Rounds.length} ${data.TotalScoreToPar}</p>
+      </div>
+      <div class="o-Leaderboard__m-Players__m-Player__a-PlayerScore">
+          <p>${data.TotalScoreToPar}</p>
+      </div>
+  </div>
+  `;
+  return html.trim();
+}
+
+function createLeaderBoard(block) {
+  const leaderboard = leaderboardTemplate({});
+  block.insertAdjacentHTML('beforebegin', leaderboard);
+  const playersEl = document.body.querySelector('.o-Leaderboard__m-Players');
+  loadJsonData(LEADERBOARD_URL).then(json => {
+    const data = json.data;
+    if (data) {
+      for (let player of data) {
+        let playerHTML = createLeaderboardPlayerHTML(player);
+        let div = document.createElement('div');
+        div.innerHTML = playerHTML;
+        playersEl.append(div.firstChild);
+      }
+    }
+  });
+}
 
 /**
  * collapses all open nav sections
@@ -23,9 +60,8 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // insert leaderboard
-  const leaderboard = leaderboardTemplate({});
-  block.insertAdjacentHTML('beforebegin', leaderboard);
-  
+  createLeaderBoard(block);
+
   // fetch nav content
   const navPath = cfg.nav || '/nav';
   const resp = await fetch(`${navPath}.plain.html`);
