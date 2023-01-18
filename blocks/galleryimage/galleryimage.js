8e1f4ebc9c245fc8f799234d1549f80990c88daf
hsaginor <hsaginor@gmail.com>
2023-01-18 11:29:56 -0800

    fixing slide counder with css

--- blocks/galleryimage/galleryimage.js
+++ blocks/galleryimage/galleryimage.js
@@ -1,5 +1,3 @@
-let blockCount = 0;
-
 function getRowClassName(field) {
     return field.trim().replace(' ', '-').toLowerCase();
 }
@@ -9,18 +7,9 @@ function decorateRow(row) {
     const className = getRowClassName(field.innerHTML);
     row.classList.add(className);
     field.remove();
-/*
-    if (className === 'promo-headline') {
-        let div = row.querySelector('div');
-        let content = div.innerHTML;
-        content = `${blockCount} ${content}`;
-        div.innerHTML = content;
-    }*/
 }
 
 export default function decorate(block) {
-    blockCount++;
-    console.log('block count = ' + blockCount);
     block.classList.add('gallery-slide');
     block.querySelectorAll(':scope > div').forEach(row => decorateRow(row));
 }
