60f69f591657f66f354b7817a65396d23b765e6d
hsaginor <hsaginor@gmail.com>
2023-02-14 10:42:01 -0800

    correct block markup

--- blocks/productlisting/productlisting.js
+++ blocks/productlisting/productlisting.js
@@ -4,30 +4,48 @@ function getRowClassName(field) {
     return field.trim().replace(' ', '-').toLowerCase();
 }
 
-function decorateRow(row, container) {
+function getOrCreatePriceWrapper(textContainer) {
+    let el = textContainer.querySelector('.price-wrapper');
+    if (!el) {
+        el = createTag('div', {class:'price-wrapper'});
+        textContainer.append(el);
+    }
+    return el;
+}
+function decorateRow(row, container, textContainer, mediaContainer) {
     const field = row.querySelector('div');
     const className = getRowClassName(field.innerHTML);
     field.remove();
     if (className === 'title') {
         let titleEl = createTag('h1', {class:'productTitle'}, row.querySelector('div').innerHTML);
         row.replaceWith(titleEl);
+        textContainer.append(titleEl);
+        row.remove();
     } else if (className === 'price') {
-        row.classList.add('price-wrapper');
+        let priceWrapper = getOrCreatePriceWrapper(textContainer);
         let priceEl = createTag('span', {class:'price'}, row.querySelector('div').innerHTML);
-        row.querySelector('div').replaceWith(priceEl);
-        row.append(createTag('span', {class:'separator'}, '|'));
+        priceWrapper.append(createTag('span', {class:'price'}, row.querySelector('div').innerHTML));
+        priceWrapper.append(createTag('span', {class:'separator'}, '|'));
+        row.remove();
     } else if(className === 'advertiser') {
-        container.querySelector('.price-wrapper').append(createTag('span', {class:className}, row.querySelector('div').innerHTML));
+        let priceWrapper = getOrCreatePriceWrapper(textContainer);
+        priceWrapper.append(createTag('span', {class:className}, row.querySelector('div').innerHTML));
+        row.remove();
+    } else if(className === 'image') {
+        mediaContainer.append(row.querySelector('picture'));
         row.remove();
     } else {
         row.classList.add(className);
+        textContainer.append(row)
     }
 }
 
 export default function decorate(block) {
+    const textContainer = createTag('div', {class: 'text-container'});
+    const mediaContainer = createTag('div', {class: 'media-container'});
     block.querySelectorAll(':scope > div').forEach(row => { 
-        if (!row.classList.contains('slide-info')) {
-            decorateRow(row, block);
-        }
+        decorateRow(row, block, textContainer, mediaContainer);
     });
+    block.append(textContainer);
+    block.append(mediaContainer);
 }
