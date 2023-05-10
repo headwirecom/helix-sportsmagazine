8d32771f609cd8d43b3cad579bed2d1eab1e91cf
icaraps <icaraps@hotmail.com>
2023-05-10 09:50:46 +0400

    v2

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,7 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './scripts.js';
-import { clearProductCache } from '../utils/utils.js';
+import { sampleRUM } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
-
-// clear products cache
-clearProductCache();
