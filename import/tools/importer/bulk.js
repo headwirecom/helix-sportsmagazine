e323b6136c8f5bdda137d80b28ee0a641fb78d4c
hsaginor <hsaginor@gmail.com>
2023-08-03 19:03:18 -0700

    extanding to execude different API operations

--- import/tools/importer/bulk.js
+++ import/tools/importer/bulk.js
@@ -6,35 +6,72 @@ console.log(log);
 const append = (string) => {
   if (log) {
     const p = document.createElement('p');
-    p.textContent = string;
+    p.innerHTML = string;
     log.append(p);
   }
   /* eslint-disable no-console */
   console.log(string);
 };
 
-const bulk = async (urls, operation, logger, startCount = 0) => {
+const updateCounter = (count, length) => {
+  if (log && log.updateCounter) {
+    log.updateCounter(count, length);
+  }
+};
+
+const bulk = async (urls, operation, logger, apiMethod = 'POST', startCount = 0) => {
   if (logger) {
     log = logger;
   }
 
   let counter = startCount;
 
+  const logIndexResponse = (path, text) => {
+    const json = JSON.parse(text);
+    const indeces = json.results;
+    let logTxt = `${counter}/${total}: ${path}`;
+    indeces.forEach((index) => {
+      if (index.record) {
+        let indexRecord = JSON.stringify(index.record);
+        logTxt = logTxt + `<br/> Index name "${index.name}": ${indexRecord}`;
+      }
+    });
+    append(logTxt);
+  }
+
+  const logStatusResponse = (path, text) => {
+    const json = JSON.parse(text);
+    let statusTxt = '{ ';
+    for (let key of Object.keys(json)) {
+      if (json[key].status && key !== 'links') {
+        statusTxt = statusTxt + `${key} status: ${json[key].status} `;
+      }
+    }
+    statusTxt = statusTxt + '}';
+    append(`${counter}/${total}: ${path} ${statusTxt}`);
+  };
+
   const executeOperation = async (url) => {
     const { hostname, pathname } = new URL(url);
     const [branch, repo, owner] = hostname.split('.')[0].split('--');
     const adminURL = `https://admin.hlx.page/${operation}/${owner}/${repo}/${branch}${pathname}`;
     const resp = await fetch(adminURL, {
-      method: 'POST',
+      method: apiMethod,
     });
+    counter += 1;
+    updateCounter(counter,total);
     const text = await resp.text();
     /* eslint-disable no-console */
+    console.log(adminURL);
     console.log(text);
-    counter += 1;
     if (resp.ok) {
-      append(`${counter}/${total}: ${adminURL}`);
+      if (operation === 'index') {
+        logIndexResponse(url, text);
+      } else {
+        logStatusResponse(url, text);
+      }
     } else {
-      append(`${counter}/${total}: FAILED ${adminURL}: ${text}`);
+      append(`${counter}/${total}: FAILED ${operation} ${apiMethod} ${adminURL}: ${text}`);
     }
   };
 
