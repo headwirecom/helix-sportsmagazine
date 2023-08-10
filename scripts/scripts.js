bf0cde59f2617dcca7329cd63ea227d217fcb89c
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2023-08-10 11:06:58 +0200

    changed prependImage function to use .page instead of .live

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -321,6 +321,6 @@ export const convertExcelDate = (excelDate) => {
  * Prepends an image path for images depending on window location
  */
 export const prependImage = (imagePath) => {
-  const host = window.location.origin.startsWith('http://localhost') ? 'https://main--helix-sportsmagazine--headwirecom.hlx.live' : window.location.origin;
+  const host = window.location.origin.startsWith('http://localhost') ? 'https://main--helix-sportsmagazine--headwirecom.hlx.page' : window.location.origin;
   return host + imagePath;
 };
