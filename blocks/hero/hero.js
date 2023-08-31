97fecb70a8433b679aaa210737f04134c3554679
laura~ <lauraavanos@hotmail.com>
2023-08-31 09:57:24 +0200

    hero dark variation

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,9 @@
 import {
-  parseFragment, render, removeEmptyElements, assignSlot, getBlockId,
+  parseFragment,
+  render,
+  removeEmptyElements,
+  assignSlot,
+  getBlockId,
 } from '../../scripts/scripts.js';
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
@@ -13,7 +17,9 @@ const placeholderHtml = '<div class="hero-container" style="visibility: hidden; 
 export default async function decorate(block) {
   const id = getBlockId(block);
 
-  const isFirstHero = document.querySelector(".hero.block[data-block-name='hero']").isEqualNode(block);
+  const isFirstHero = document
+    .querySelector('.hero.block[data-block-name="hero"]')
+    .isEqualNode(block);
 
   const heroDataIndex = heroItemsIndex;
   heroItemsIndex += isFirstHero ? 5 : 1;
@@ -33,14 +39,20 @@ export default async function decorate(block) {
 
     assignSlot(block, 'image', 'picture');
 
-    const cardsTemplate = cards
-      .map(
-        (card) => `
-              <a href="${card.path}">
+    let cardsTemplate = '';
+
+    if (!block.classList.contains('no-cards')) {
+      cardsTemplate = cards
+        .map(
+          (card) => `
+              <a href='${card.path}'>
                 ${
-  createOptimizedPicture(card.image, card.imageAlt || card.title, isFirstHero, [
-    { width: '120' },
-  ]).outerHTML
+  createOptimizedPicture(
+    card.image,
+    card.imageAlt || card.title,
+    isFirstHero,
+    [{ width: '120' }],
+  ).outerHTML
 }
                 <div>
                   <span>${card.rubric}</span>
@@ -48,42 +60,65 @@ export default async function decorate(block) {
                 </div>
               </a>
             `,
-      )
-      .join('');
+        )
+        .join('');
+    }
 
-    const withCards = isFirstHero && cards.length;
-    if (withCards) {
-      block.classList.add('with-cards');
+    if (!block.classList.contains('no-cards')) {
+      if (isFirstHero && cards.length) {
+        block.classList.add('with-cards');
+      } else {
+        block.classList.add('no-cards');
+      }
     }
 
     // HTML template in JS to avoid extra waterfall for LCP blocks
     const HTML_TEMPLATE = `
-        <div class="hero-container">
-          <div class="hero-image-container">
-            ${
-  createOptimizedPicture(heroData.image, heroData.imageAlt || heroData.title, isFirstHero, [
-    { media: '(min-width: 686px)', width: '686' },
-    { media: '(min-width: 966px)', width: '966' },
-    { media: '(min-width: 1280px)', width: '1280' },
-    { width: '1850' },
-  ]).outerHTML
+    <div class="hero-container">
+        <div class="hero-image-container">
+        ${
+  createOptimizedPicture(
+    heroData.image,
+    heroData.imageAlt || heroData.title,
+    isFirstHero,
+    [
+      { media: '(min-width: 686px)', width: '686' },
+      { media: '(min-width: 966px)', width: '966' },
+      { media: '(min-width: 1280px)', width: '1280' },
+      { width: '1850' },
+    ],
+  ).outerHTML
 }
-          </div>
-          <div class="hero-text-container">
-            <a href="${heroData.path}">
-              <div>
-                <${isFirstHero ? 'h1' : 'h2'}>${heroData.rubric}</${isFirstHero ? 'h1' : 'h2'}>
-                <p>${heroData.title}</p>
-              </div>
-              <div class="button-container">
-                <div class="icon-container">${arrowIcon}</div>
-                <span>Read story</span>
-              </div>
-            </a>
-          </div>
-          ${withCards ? `<div class="hero-cards-container">${cardsTemplate}</div>` : ''}
         </div>
-      `;
+        <div class="hero-text-container">
+        <a href='${heroData.path}'>
+            <div>
+            <${isFirstHero ? 'h1' : 'h2'}>${
+  block.classList.contains('no-cards') ? heroData.title : heroData.rubric
+}</${isFirstHero ? 'h1' : 'h2'}>
+            ${
+  block.classList.contains('no-cards')
+    ? ''
+    : `<p>${heroData.title}</p>`
+}
+            </div>
+            <div class="button-container">
+            <div class="icon-container">${arrowIcon}</div>
+            <span>${
+  block.classList.contains('no-cards')
+    ? 'Watch series'
+    : 'Read story'
+}</span>
+            </div>
+        </a>
+        </div>
+        ${
+  cardsTemplate
+    ? `<div class="hero-cards-container">${cardsTemplate}</div>`
+    : ''
+}
+    </div>
+`;
 
     // Parse the HTML template into a document fragment
     const template = parseFragment(HTML_TEMPLATE);
@@ -95,7 +130,10 @@ export default async function decorate(block) {
     removeEmptyElements(template, 'p');
 
     // Default text alignment
-    if (!block.classList.contains('text-center') && !block.classList.contains('text-right')) {
+    if (
+      !block.classList.contains('text-center')
+      && !block.classList.contains('text-right')
+    ) {
       block.classList.add('text-left');
     }
 
