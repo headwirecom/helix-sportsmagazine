30f94433c10f190afb0f082d739ab913c756e9c1
icaraps <icaraps@hotmail.com>
2023-09-08 15:22:16 +0400

    add vanity url bulk import gh action

---
+++ bin/bulk-import.js
@@ -0,0 +1,29 @@
+export default async function bulkImport({ github, fetch }) {
+  const { name, offset, limit } = github.event.inputs;
+  const origin = 'https://gd.headwire.workers.dev';
+  const reqIndex = await fetch(`${origin}/${name}-query-index.json?sheet=latest&offset=${offset}&limit=${limit}`);
+
+  if (!reqIndex.ok) {
+    console.log(`${reqIndex.status}: ${reqIndex.text()}`);
+    return;
+  }
+
+  const { data } = reqIndex.json();
+  for (const { path } of data) {
+    // eslint-disable-next-line no-await-in-loop
+    const reqImport = await fetch(`${origin}/webhook`, {
+      method: 'POST',
+      body: `${path}.md`,
+      headers: {
+        Authorization: `Bearer ${process.env.GD_TOKEN}`,
+      },
+    });
+
+    if (!reqImport.ok) {
+      console.log(`${reqImport.status}: ${reqImport.text()}`);
+      break;
+    }
+
+    console.log(`Imported ${path} successfully!`);
+  }
+}
